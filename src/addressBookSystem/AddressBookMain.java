5a9a3bf364bfb6594d60462d40e733bfb51475b3
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-06-13 12:46:32 +0530

    updating changes

--- src/addressBookSystem/AddressBookMain.java
+++ src/addressBookSystem/AddressBookMain.java
@@ -1,5 +1,34 @@
 package addressBookSystem;
 
 public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to address book program");
+		
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Contact Details:");
+        System.out.print("First Name: ");
+        String firstName = scanner.nextLine();
+        System.out.print("Last Name: ");
+        String lastName = scanner.nextLine();
+        System.out.print("Address: ");
+        String address = scanner.nextLine();
+        System.out.print("City: ");
+        String city = scanner.nextLine();
+        System.out.print("State: ");
+        String state = scanner.nextLine();
+        System.out.print("Zip: ");
+        String zip = scanner.nextLine();
+        System.out.print("Phone Number: ");
+        String phoneNumber = scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+        System.out.println("\nContact Created Successfully:");
+        System.out.println(contact);
+		
+	}
 
 }
